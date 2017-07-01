15adce1c47cc93590413a614e238e5a4048d7487
Razip <buyanov.dmitriy.v@yandex.com>
2017-07-01 13:28:47 +0500

    Closes #6 The code has been reformatted

--- modalWindow.js
+++ modalWindow.js
@@ -125,7 +125,7 @@ ModalWindow.prototype.unlock = function () {
     return this;
 };
 
-ModalWindow.prototype.allowMoving = function() {
+ModalWindow.prototype.allowMoving = function () {
     this.movable = true;
 };
 
@@ -185,6 +185,11 @@ ModalWindow.prototype.setContent = function (content, centralizeMovable, keepMov
             this.content.innerHTML = content;
         }
     }
+
+    this.window.style.width = this.window.style.height = '';
+
+    this.window.style.width = this.window.clientWidth + 'px';
+    this.window.style.height = this.window.clientHeight + 'px';
 };
 
 ModalWindow.prototype.onCloseCallback = function () {
